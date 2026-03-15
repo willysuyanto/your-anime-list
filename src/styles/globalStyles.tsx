f379097db5ab79c3cf90d9894711cc35c5980395
Willy Suyanto <willy.suyanto121212@gmail.com>
2026-03-15 16:16:50 +0700

    fix(): fix styled components import

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
 
   *,
   *::before,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
@@ -9,7 +8,6 @@ export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    tsconfigPaths(),
     babel({ presets: [reactCompilerPreset()] })
   ],
 })
